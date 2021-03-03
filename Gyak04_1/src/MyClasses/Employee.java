24e323b06144889fc9f23412c32a7025b6f4fccb
erika <erika@erika-Vostro-3350>
2021-03-03 13:26:31 +0100

    4. gyakorlat 1. feladat 'c' verzió

--- Gyak04_1/src/MyClasses/Employee.java
+++ Gyak04_1/src/MyClasses/Employee.java
@@ -4,15 +4,18 @@ public class Employee {
 	private String name;
 	private int salary;
 	
+	// a)
 	public void increaseSalary(int value) {
 		salary += value;
 	}
 	
 	public String displayInfo() {
 		//increaseSalary(100);
-		return "Név: " + name + ", fizetés: " + salary;
+		return "Név: " + name + ", fizetés: " + salary +
+				", adó: " + getTax();
 	}
 
+	// b)
 	public String getName() {
 		return name;
 	}
@@ -30,4 +33,27 @@ public class Employee {
 			this.salary = salary;
 	}
 
+	// c)
+	public boolean isInRange(int lower, int upper) {
+		if (this.salary <= upper && this.salary >= lower)
+			return true;
+		return false;
+	}
+	
+	private double getTax() {
+		return Math.round(this.salary*0.16);
+	}
+	
+	public boolean hasHigherSalary(Employee e) {
+		if (this.salary > e.salary)
+			return true;
+		return false;
+	}
+	
+	public Employee compareSalary(Employee e) {
+		if (this.salary > e.salary)
+			return this;
+		return e;
+	}
+	
 }
--- Gyak04_1/src/Tests/EmployeeTest.java
+++ Gyak04_1/src/Tests/EmployeeTest.java
@@ -6,12 +6,25 @@ public class EmployeeTest {
 
 	public static void main(String[] args) {
 		
-		Employee emp = new Employee();
-		emp.setName("John");
-		emp.setSalary(-4);
-		System.out.println(emp.displayInfo());
-		emp.increaseSalary(1000);
-		System.out.println(emp.displayInfo());
+		Employee emp1 = new Employee();
+		emp1.setName("John");
+		emp1.setSalary(10000);
+		
+		Employee emp2 = new Employee();
+		emp2.setName("Jane");
+		emp2.setSalary(15000);
+		
+		System.out.println(emp1.displayInfo());
+		System.out.println(emp2.displayInfo());
+		
+		boolean res = emp1.hasHigherSalary(emp2);
+		if (res)
+			System.out.println(emp1.getName() + " fizetése nagyobb");
+		else
+			System.out.println(emp1.getName() + " fizetése kisebb");
+		
+		System.out.println(emp2.compareSalary(emp1).getName());
+		
 	}
 
 }
